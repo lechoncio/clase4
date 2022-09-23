3ce6e286767130d954453e07c07c693c9c0685be
Rafael Ketelhohn <rafaelk@gmail.com>
2022-09-23 13:45:47 -0300

    Desafío: Item Collection II

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,55 @@
+
+import { serverTimestamp, doc, collection, setDoc, increment, updateDoc } from "firebase/firestore";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import { db } from "../utils/firebaseConfig";
+
+
 
 const Cart = () => {
     
     const test = useContext(CartContext);
-    
 
+    const createOrder = () => {
+        let itemsForDB = test.cartList.map(item => ({
+            id: item.id,
+            title: item.brand,
+            model: item.nombre,
+            price: item.precio,
+            quantity: item.quantity
+
+                }))
+        let order = {
+            comprador:{
+                name: "Alex Trujillo",
+                email: "atrujillo@gmail.com",
+                phone: "11 3819 6536"
+            },
+            date: serverTimestamp(),
+            items: itemsForDB,
+            total: test.calcTotalCompra()           
+        }        
+        const createOrderInFirestore = async () => {
+            const newOrderRef = doc(collection(db, "orders"));
+            await setDoc(newOrderRef, order);
+            return newOrderRef;
+        }
+
+        createOrderInFirestore()
+            .then(result => {
+                alert("Champion, Your order has been created!" + result.id)
+                test.cartList.forEach(async(item) => {
+                const itemRef = doc(db, "products", item.id);
+                await updateDoc(itemRef, {
+                stock: increment(-item.quantity)
+                });
+            })
+            test.clear()
+            })
+            .catch(err => console.log(err))
+
+
+    }
 
     return (
         <>
@@ -38,7 +82,9 @@ const Cart = () => {
         </div>
                 
         <div class="flex-item"><h1>Total Compra = {test.calcTotalCompra()} pesos . </h1> </div>
-        <div class="flex-item"><buttom class="btn btn-secondary btn-lg"> Realizar Compra</buttom></div>
+
+        <div class="flex-item"><buttom class="btn btn-secondary btn-lg" onClick={createOrder}> Realizar Compra</buttom></div>
+
         <div class="flex-item"><button class="btn btn-outline-danger btn-lg" onClick={test.clear}>Quitar todos</button></div>
         
         </div>
